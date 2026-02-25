43d7b623ef35e1b3006c8f88e9deeacafdde0021
Jef Mayer <jef.a.mayer@gmail.com>
2026-02-25 11:36:49 -0700

    stubbed in zerostep, example

---
+++ playwright/websites/jefmayer.com/portfolio-nav-anchor-ai.spec.ts
@@ -0,0 +1,16 @@
+import { test } from '@playwright/test';
+import { expectInViewport } from '@utils/viewport-utils';
+import * as dotenv from 'dotenv';
+import { ai } from '@zerostep/playwright'
+
+dotenv.config();
+
+test('Portfolio Nav Anchor', async ({ page }) => {
+  await page.goto(process.env.BASE_URL, { waitUntil: 'load' });
+  await ai('Wait for the page to load and the down arrow button to appear', { page, test });
+  await ai('Click the down arrow button', { page, test });
+  await ai('Click the menu button in the top right corner of the page', { page, test });
+  await ai('Click the link that contains Tumblr', { page, test });
+  await page.waitForTimeout(1500);
+  await expectInViewport(page.getByRole('heading', { name: 'Tumblr' }).first(), false);
+});
